457862fcae921b513d20d0d2375c4833ef517df7
Iris Huang <lyaui@outlook.com>
2024-01-27 01:37:20 +0800

    style(Page): this.style(Page)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
-import Image from 'next/image';
+// import H1 from '@/components/h1';
+// import SearchForm from '@/components/search-form';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      home
+    <main className='flex flex-col items-center px-3 pt-36'>
+      {/* <H1>Find events around you</H1> */}
+
+      <p className='mb-12 mt-7 text-2xl lg:text-3xl opacity-75'>
+        Browse more than{' '}
+        <span className='font-bold text-[#a4f839] text-accent italic underline'>
+          10,000 events
+        </span>{' '}
+        around you
+      </p>
+
+      <from className='w-full sm:w-[580px]'>
+        <input
+          className='w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10'
+          placeholder='Search events in any city...'
+          spellCheck={false}
+        />
+      </from>
+
+      <section className='mt-4 flex gap-x-4 text-sm text-white/50'>
+        <p>Popular:</p>
+        <div className='space-x-2 font-semibold'>
+          <Link href='/events/austin'>Austin</Link>
+          <Link href='/events/seattle'>Seattle</Link>
+        </div>
+      </section>
     </main>
   );
 }
