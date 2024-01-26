3480efc7f2db343cbe9a9d1687f18b6864a7ec7a
Iris Huang <lyaui@outlook.com>
2024-01-27 01:28:40 +0800

    feat(Header): add links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,28 @@
-import Link from 'next/link';
-import React from 'react';
+'use client';
 
-function Header() {
+import Link from 'next/link';
+import Logo from '@/components/Logo';
+
+const routes = [
+  { name: 'Home', path: '/' },
+  { name: 'All Event', path: '/events/all' },
+];
+export default function Header() {
   return (
-    <header>
-      <Link href='/'>Home</Link>
-      <Link href='/events/all'>All Events</Link>
+    <header className='flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9'>
+      <Logo />
+      <nav className='h-full'>
+        <ul className='flex gap-x-6 h-full text-sm'>
+          {routes.map((_route) => (
+            <li
+              className='text-white/50 hover:text-white transition'
+              key={_route.path}
+            >
+              <Link href={_route.path}>{_route.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
-
-export default Header;
