db84319705bda448bf579ceea6861df7d0776d08
Iris Huang <lyaui@outlook.com>
2024-01-27 00:58:04 +0800

    style(Layout): add class name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body
+        className={`${inter.className} bg-gray-950 text-white overflow-y-scroll`}
+      >
         <Header />
         {children}
         <Footer />
