e0e47fccb028e7f6f4ef4253db07c1036fc7c442
Iris Huang <lyaui@outlook.com>
2024-01-27 17:31:35 +0800

    feat(EventsPage): title params

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,13 +1,17 @@
 import H1 from '@/components/H1';
 
-function page() {
+type EventsPageProps = {
+  params: { city: string };
+};
+function EventsPage({ params }: EventsPageProps) {
+  const city = params.city.charAt(0).toUpperCase() + params.city.slice(1);
   return (
     <main>
       <H1 className='flex flex-col items-center px-3 pt-36'>
-        Events in Austin
+        {params.city === 'all' ? `All Events` : `Events in ${city}`}
       </H1>
     </main>
   );
 }
 
-export default page;
+export default EventsPage;
