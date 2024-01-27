ac2f786a00ae1f97cbeb32c40642f86701fe5775
Iris Huang <lyaui@outlook.com>
2024-01-27 17:15:36 +0800

    feat(EventsPage): add title

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import H1 from '@/components/H1';
 
 function page() {
-  return <main>page</main>;
+  return (
+    <main>
+      <H1 className='flex flex-col items-center px-3 pt-36'>
+        Events in Austin
+      </H1>
+    </main>
+  );
 }
 
 export default page;
