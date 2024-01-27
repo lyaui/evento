c90b7f0fe99f4af4c825dc34eee5c3a6712ce87a
Iris Huang <lyaui@outlook.com>
2024-01-27 13:11:56 +0800

    style(Header): active path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import { motion } from 'framer-motion';
+
 import Logo from '@/components/Logo';
 
 const routes = [
@@ -8,17 +12,33 @@ const routes = [
   { name: 'All Event', path: '/events/all' },
 ];
 export default function Header() {
+  const currentPath = usePathname();
+
   return (
     <header className='flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9'>
       <Logo />
+
       <nav className='h-full'>
         <ul className='flex gap-x-6 h-full text-sm'>
           {routes.map((_route) => (
             <li
-              className='text-white/50 hover:text-white transition'
               key={_route.path}
+              className={clsx(
+                'hover:text-white flex items-center relative transition',
+                {
+                  'text-white': currentPath === _route.path,
+                  'text-white/50': currentPath !== _route.path,
+                },
+              )}
             >
               <Link href={_route.path}>{_route.name}</Link>
+
+              {currentPath === _route.path && (
+                <motion.div
+                  layoutId='header-active-link'
+                  className='bg-accent h-1 w-full absolute bottom-0'
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
